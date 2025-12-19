a7c19d9499ba74bd1c6b2d954959609a6e955d50
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-19 13:43:19 +0100

    ajout d'une méthode pour générer automatiquement un numéro de compte valide

--- src/service/GestionnaireComptes.java
+++ src/service/GestionnaireComptes.java
@@ -5,6 +5,7 @@ import model.CompteBancaire;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class GestionnaireComptes {
     private Map<String, CompteBancaire> comptes;
@@ -16,6 +17,17 @@ public class GestionnaireComptes {
         chargerComptes();
     }
 
+    private String genererNumeroCompte(){
+        Random rand = new Random();
+        String numero;
+        do {
+            int partie1 = 1000 + rand.nextInt(1000);
+            int partie2 = 1000 + rand.nextInt(1000);
+            numero = String.format("FR-%04d-%04d", partie1, partie2);
+        } while (comptes.containsKey(numero));
+        return numero;
+    }
+
     public CompteBancaire creerCompte(String titulaire, double soldeInitial) {
         String numeroCompte = genererNumeroCompte();
         CompteBancaire compte = new CompteBancaire(numeroCompte, titulaire, soldeInitial);
@@ -23,6 +35,7 @@ public class GestionnaireComptes {
         stockage.sauvegarderCompte(compte);
         return compte;
     }
+
 
     private void chargerComptes(){
         this.comptes = stockage.chargerComptes();
