5a96987556dc46eb67465076cd52f1e56d747ef2
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-19 11:38:43 +0100

    ajout d'un attribut de pattern pour le format du numéro de compte FR-XXXX-XXXX

--- src/model/CompteBancaire.java
+++ src/model/CompteBancaire.java
@@ -1,9 +1,13 @@
 package model;
 
+import java.util.regex.Pattern;
+
 public class CompteBancaire {
     private String numeroCompte;
     private String titulaire;
     private double solde;
     private Double plafond;
     private List<Operation> historique;
+
+    private static final Pattern PATTERN_NUMERO_COMPTE = Pattern.compile("^FR-\\d{4}-\\d{4}$");
 }
