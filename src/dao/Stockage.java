704bd33f444b26a10734c0480daa26fbc6efbb87
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-19 13:32:12 +0100

    ajout des méthode de l'interface

--- src/dao/Stockage.java
+++ src/dao/Stockage.java
@@ -1,4 +1,14 @@
 package dao;
 
+import model.CompteBancaire;
+import model.Operation;
+
+import java.util.Map;
+
 public interface Stockage {
+    void sauvegarderCompte(CompteBancaire compte);
+    Map<String, CompteBancaire> chargerComptes();
+    void sauvegarderOperation(Operation operation? String numeroCompteDestination);
+    void mettreAjourSolde(CompteBancaire compte);
+    void mettreAjourPlafond(CompteBancaire compte);
 }
