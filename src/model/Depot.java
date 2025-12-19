371dfbaf8659cfa77be341e44d19425aa60a7182
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-19 12:46:53 +0100

    ajout de la classe depot + surcharge de la méthode éxecuter

---
+++ src/model/Depot.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Depot extends Operation{
+    public Depot(double montant, CompteBancaire compte) {
+        super(montant, compte, TypeOperation.DEPOT);
+    }
+    
+    @Override
+    public void executer(){
+        getCompteSource().deposer(getMontant());
+        getCompteSource().ajouterOperation(this);
+
+    }
+}
