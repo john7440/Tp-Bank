cd6e292c76dd64c798f263a61f3f520b9f7e9d41
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-19 13:36:26 +0100

    ajout d'une méthode pour charger les comptes

--- src/service/GestionnaireComptes.java
+++ src/service/GestionnaireComptes.java
@@ -1,4 +1,21 @@
 package service;
 
+import dao.Stockage;
+import model.CompteBancaire;
+
+import java.util.HashMap;
+
 public class GestionnaireComptes {
+    private Map<String, CompteBancaire> comptes;
+    private Stockage stockage;
+
+    public GestionnaireComptes(Stockage stockage) {
+        this.comptes = new HashMap<>();
+        this.stockage = stockage;
+        chargerComptes();
+    }
+
+    private void chargerComptes(){
+        this.comptes = stockage.chargerComptes();
+    }
 }
