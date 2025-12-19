13f7a1e4b8d58fd5ae9937c7eb0096447f3fd0b7
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-19 11:42:45 +0100

    génération des getters et des setters

--- src/model/CompteBancaire.java
+++ src/model/CompteBancaire.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class CompteBancaire {
@@ -10,4 +12,42 @@ public class CompteBancaire {
     private List<Operation> historique;
 
     private static final Pattern PATTERN_NUMERO_COMPTE = Pattern.compile("^FR-\\d{4}-\\d{4}$");
+
+    //Getters et Setters
+    public String getNumeroCompte() {
+        return numeroCompte;
+    }
+
+    public void setNumeroCompte(String numeroCompte) {
+        this.numeroCompte = numeroCompte;
+    }
+
+    public String getTitulaire() {
+        return titulaire;
+    }
+
+    public void setTitulaire(String titulaire) {
+        this.titulaire = titulaire;
+    }
+
+    public double getSolde() {
+        return solde;
+    }
+
+    public void setSolde(double solde) {
+        this.solde = solde;
+    }
+
+    public Double getPlafond() {
+        return plafond;
+    }
+
+    public void setPlafond(Double plafond) {
+        this.plafond = plafond;
+    }
+
+    public List<Operation> getHistorique() {
+        return new ArrayList<>(historique);
+    }
+
 }
