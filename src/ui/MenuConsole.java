2e7ac7257777eaa3ac0f28b0e9b15e863ce6f750
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-19 15:13:00 +0100

    ajout d'attributs + constructeur

--- src/ui/MenuConsole.java
+++ src/ui/MenuConsole.java
@@ -1,4 +1,15 @@
 package ui;
 
+import service.GestionnaireComptes;
+
+import java.util.Scanner;
+
 public class MenuConsole {
+    private GestionnaireComptes gestionnaire;
+    private Scanner scan;
+
+    public MenuConsole(GestionnaireComptes gestionnaire) {
+        this.gestionnaire = gestionnaire;
+        this.scan = new Scanner(System.in);
+    }
 }
