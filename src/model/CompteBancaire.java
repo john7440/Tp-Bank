3fc8ab772665e755d9a97c7a3f651ca21463d6aa
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-12-19 11:33:03 +0100

    ajout de la classe compte bancaire avec ses attributs

---
+++ src/model/CompteBancaire.java
@@ -0,0 +1,9 @@
+package model;
+
+public class CompteBancaire {
+    private String numeroCompte;
+    private String titulaire;
+    private double solde;
+    private Double plafond;
+    private List<Operation> historique;
+}
